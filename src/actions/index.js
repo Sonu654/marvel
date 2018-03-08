b022cf0c4c5adf2c351943cc85fe67b28a25b640
Suraj Sanwal <sonusanwal65@gmail.com>
2018-03-08 18:45:08 +0530

    eslint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,10 @@
-export const LAUNCH = 'LAUNCH';
-export const USER_AVAILABLE = 'USER_AVAILABLE';
-export const USER_FOUND = 'USER_FOUND';
-import Data from '../users.json';
-import { Actions } from 'react-native-router-flux';
-//import Data from '../instruction.json';
+export const LAUNCH = "LAUNCH";
+export const USER_AVAILABLE = "USER_AVAILABLE";
+export const USER_FOUND = "USER_FOUND";
+import Data from "../users.json";
+import { Actions } from "react-native-router-flux";
+//import Data from"../instruction.json";
 //                   instruction.json
-
 export function launch() {
     return (dispatch) => {
         setTimeout(() => {
@@ -15,7 +14,7 @@ export function launch() {
 }
 
 export function getUser() {
-    console.log('inside getUser');
+    console.log("inside getUser");
     return (dispatch) => {
         setTimeout(() => {
             console.log(Data);
@@ -26,18 +25,18 @@ export function getUser() {
 
 
 export function getUserByName(uname) {
-    console.log('inside getUserByName');
+    console.log("inside getUserByName");
     users = Data.users;
     return (dispatch) => {
         for (let user in users) {
-           // console.log('user : ', user);
+            // console.log("user :", user);
             if (users[user].uname == uname) {
                 userData = {
                     "uname": uname,
                     "name": users[user].name,
                     "userImg": users[user].userImg
                 };
-                console.log('Contact : ', userData);
+                console.log("Contact :", userData);
                 dispatch({ type: USER_FOUND, data: userData });
             }
         }
@@ -45,17 +44,17 @@ export function getUserByName(uname) {
 }
 
 
-export function addPerson(name, age, pic, tag){
-     contact={
-        uname:name+age,
-        age:age,
+export function addPerson(name, age, pic, tag) {
+    contact = {
+        uname: name + age,
+        age: age,
         name: name,
-        userImg:pic,
+        userImg: pic,
         tags: tag
     };
     Data.users.push(contact);
-    console.log("pushed data : ",Data );
-    return(dispatch)=>{
+    console.log("pushed data : ", Data);
+    return (dispatch) => {
         dispatch({ type: USER_AVAILABLE, data: Data });
         Actions.New_home_housemates();
     }
